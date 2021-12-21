7fd91f9d462839075a7129bcc8d10958a7b66bf3
gabhijeetaxioned <abhijeetg@axioned.com>
2021-12-21 18:53:43 +0530

    fix: In product sort added the select filter

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,13 @@
 import { useCallback, useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchProduct, sortProduct } from "../actions";
+import { fetchProduct } from "../actions";
 import Loader from "./Loader";
 import DisplayProduct from "./DisplayProduct";
 import '../assets/css/Home.css'
 
 const Home = () => {
 
-    const [category,setcategory] = useState(['All']);
+    const [productLists, setProductLists] = useState([]);
     const [limit, setlimit] = useState(4);
     const dispatch = useDispatch();
     const getProducts = useSelector(state => state.products.array);
@@ -15,18 +15,14 @@ const Home = () => {
     useEffect(() => {
         dispatch(fetchProduct());
     }, []);
+
+    useEffect(() => {
+        setProductLists(getProducts);
+    }, [getProducts]);
     
-    const sort = value => {
-        if(!category.includes((value))) {
-            setcategory([...category,value]);
-            dispatch(sortProduct(value));
-        }
-    }
-    
-    const clearSort = () => {
-        setcategory(['All']);
-        setlimit(4);
-        dispatch(fetchProduct());
+    const sortValue = value => {
+        if(value === 'all') setProductLists(getProducts);
+        else setProductLists(getProducts.filter(product => product.category === value));
     }
 
     const observer = useRef();
@@ -46,20 +42,22 @@ const Home = () => {
                 <div className='available-products'>
                     <h2>Available Products</h2>
                     <div className="sort">
-                        <span>{category.map((catg,index) => {
-                            if(index === 0) return catg;
-                            return ' > ' + catg;
-                        })}</span>
-                        <button onClick={() => clearSort()}>Clear Sort</button>
+                        <select defaultValue='All' onChange={e => sortValue(e.target.value)}>
+                            <option value="all">All</option>
+                            <option value="men's clothing">Men's Clothing</option>
+                            <option value="women's clothing">Women's Clothing</option>
+                            <option value="jewelery">Jewelery</option>
+                            <option value="electronics">Electronics</option>
+                        </select>
                     </div>
                 </div>
                 <ul className='products'>
-                    {getProducts.length < 1 ? <div className="loader-wrapper"><Loader /></div> : getProducts.map((product,key) => {
+                    {productLists.length < 1 ? <div className="loader-wrapper"><Loader /></div> : productLists.map((product,key) => {
                         if(key < limit) {
                             if(limit === key+1) {
-                                return <li ref={lastProductRef} key={key}><DisplayProduct product={product} sortValue={(value) => sort(value)} /></li>
+                                return <li ref={lastProductRef} key={key}><DisplayProduct product={product} /></li>
                             }
-                            return <li key={key}><DisplayProduct product={product} sortValue={(value) => sort(value)} /></li>
+                            return <li key={key}><DisplayProduct product={product} /></li>
                         }  
                         return null
                     })}
