c62a043ba8771d106e44e5822adaa17dc17df651
gabhijeetaxioned <abhijeetg@axioned.com>
2021-12-22 23:09:47 +0530

    feat: styled-components package added

---
+++ src/component/Login/index.js
@@ -0,0 +1,15 @@
+import { LoginForm } from "./login.style";
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="wrapper">
+                <LoginForm>
+                    
+                </LoginForm>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/component/Login/login.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LoginForm = styled.form`
+    width: 40%
+    margin: 20px auto;
+    background-color: #fff;
+`;
